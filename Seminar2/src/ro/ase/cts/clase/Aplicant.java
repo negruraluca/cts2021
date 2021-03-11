22fcfaf15e451aa6d730a1882b3bd8403ba0a5a2
Ralu <Ralu@DESKTOP-SMTDP1I>
2021-03-11 16:09:13 +0200

    modificari seminar 3

--- Seminar2/src/ro/ase/cts/clase/Aplicant.java
+++ Seminar2/src/ro/ase/cts/clase/Aplicant.java
@@ -9,6 +9,12 @@ public abstract class Aplicant{
 	protected String[] denumireProiect;
 	
 	
+	public String[] getDenumireProiect() {
+		return denumireProiect;
+	}
+	public void setDenumireProiect(String[] denumireProiect) {
+		this.denumireProiect = denumireProiect;
+	}
 	public String getNume() {
 		return nume;
 	}
@@ -59,8 +65,9 @@ public abstract class Aplicant{
 	public int getNr_proiecte() {
 		return nr_proiecte;
 	}
-	public void setNr_proiecte(int nr_proiecte) {
+	public void setDenumireProiecte(int nr_proiecte,String[] denumire_proiecte) {
 		this.nr_proiecte = nr_proiecte;
+		this.denumireProiect=denumire_proiecte;
 	}
 
 }
--- Seminar2/src/ro/ase/cts/clase/readers/AplicantReader.java
+++ Seminar2/src/ro/ase/cts/clase/readers/AplicantReader.java
@@ -2,6 +2,7 @@ package ro.ase.cts.clase.readers;
 
 import java.io.FileNotFoundException;
 import java.util.List;
+import java.util.Scanner;
 
 import ro.ase.cts.clase.Aplicant;
 
@@ -15,4 +16,22 @@ public abstract class AplicantReader {
 	}
 	
 	public abstract List<Aplicant> readAplicanti() throws FileNotFoundException;
+
+	public void readAplicant(Scanner scanner, Aplicant aplicant)
+	{
+		String nume = scanner.next();
+		String prenume = scanner.next();
+		int varsta = scanner.nextInt();
+		int punctaj = scanner.nextInt();
+		int nr = scanner.nextInt();
+		String[] vect = new String[5];
+		for (int i = 0; i < nr; i++)
+			vect[i] = scanner.next();
+		aplicant.setNume(nume);
+		aplicant.setPrenume(prenume);
+		aplicant.setPunctaj(punctaj);
+		aplicant.setVarsta(varsta);
+		aplicant.setDenumireProiecte(nr,vect);
+		
+	}
 }
